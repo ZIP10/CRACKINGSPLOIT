908382ae6f709d0e3e1bad0da45427f13cae5ca9
ZIP10 <41266528+ZIP10@users.noreply.github.com>
2018-07-16 19:12:33 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+
+
+echo " 
+        °°°°°°°     ___  ___ _______
+        °     °        ||       ()
+        °     °        ||       ()
+        °°°°°°°        ||       ()
+              °        ||       ()
+              °        ||       ()
+        °°°°°°°     ___||___    ()"
+        
+        
+git clone https://github.com/juat4you/Crackingsploit.git
+
+ls
+
+exit
