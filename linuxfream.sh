aa0e628ac3897b2f36f4f4e5c065f207a1929d5e
ZIP10 <41266528+ZIP10@users.noreply.github.com>
2018-07-16 19:06:01 +0530

    Add files via upload

---
+++ linuxfream.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+echo "\033[1;32m welcome to hackingsploit"
+
+
+apt install nmap -y
+echo "   
+      ________________________________"
+echo "
+      |                               |
+      |            |\    |            |
+      |            | \   |            |
+      |            |  \  |            |
+      |            |   \ |            |
+      |            |    \|            | "
+ echo "      |-----—-------------------------|
+      |===============================|
+      |\      /|        /\        ______
+      | \    / |       /  \       |     )
+      |  \  /  |      /____\      |_____)
+      |   \/   |     /      \     |
+      |________|____/________\____|_____"
+      
+echo " \033[1;37m Enter scan the website name $person"
+read person
+nmap 
+
+nmap $person
+read person
+
+exit
